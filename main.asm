27e78f2d686b7f18b704e096fead2fd032f8f74d
ShayakSarkar <shayaksarkar2000@gmail.com>
2019-12-02 10:53:35 +0530

    Minor changes

--- main.asm
+++ main.asm
@@ -1,11 +1,12 @@
-[org 0x7c00]	; this is important because the boot sector code originates 		    ; from this address. Therefore if this is not specified
+[org 0x7c00]	; this is important because the boot sector code originates 		    
+		; from this address. Therefore if this is not specified
 		; then the bytes we define will be defined elsewhere in the
 		; memory. 
 
 call _print
 jmp $
 
-_print:		;print whatever is in the buffer
+_print:			;print whatever is in the buffer
 	mov bx,buffer
 	mov cl,0	
 	P_loop_label1:	
@@ -24,7 +25,8 @@ _print:		;print whatever is in the buffer
 	mov al,cl
 	ret
 
-buffer db "Shayak is awesome",0x00	; it is important to define addition					    ; al bytes later on because the bios
+buffer db "Shayak is awesome",0x00	; it is important to define addition					    
+					; al bytes later on because the bios
 					; will try to execute whatever bytes
 					; are loaded into the ram. Therefore
 					; the normally defined bytes might
